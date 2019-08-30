fdeff3d3ebb88e636f4e63796e9c17a4fac1ee27
Mike <mmcshane10@gmail.com>
2019-08-30 09:12:30 -0700

    Add UI js for form submission and object creation

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,36 @@
 // BUSINESS LOGIC -----------------------------------------
 
-//Pizza Constructor
-function Pizza(size, meat, toppings, price) {
+// Pizza Constructor
+function Pizza(size, meat, toppings) {
   this.size = size,
   this.meat = meat,
   this.toppings = toppings,
-  this.price = price
+  this.price = 0
 };
 
+var meatInput = [];
+var toppingsInput = [];
+
 // USER LOGIC ---------------------------------------------
+
+// Form
+$(document).ready(function() {
+  $("form#pizza_builder").submit(function(event) {
+    event.preventDefault();
+    var sizeInput = $("input:radio[name=pizza-size]:checked").val();
+
+    $("input:checkbox[name=toppings-meat]:checked").each(function(){
+      var userMeatInput = $(this).val();
+      meatInput.push(userMeatInput);
+    });
+
+    $("input:checkbox[name=toppings-additional]:checked").each(function(){
+      var userToppingsInput = $(this).val();
+      toppingsInput.push(userToppingsInput);
+    });
+
+    var newPizza = new Pizza(sizeInput, meatInput, toppingsInput);
+
+    console.log(newPizza);
+  });
+});
