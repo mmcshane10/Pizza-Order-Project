1051b573e73f6373fff09b51491b0fec77ba377e
Mike <mmcshane10@gmail.com>
2019-08-30 08:46:00 -0700

    add sidebar and form

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// BUSINESS LOGIC -----------------------------------------
+
+//Pizza Constructor
+function Pizza(size, meat, toppings, price) {
+  this.size = size,
+  this.meat = meat,
+  this.toppings = toppings,
+  this.price = price
+};
+
+// USER LOGIC ---------------------------------------------
