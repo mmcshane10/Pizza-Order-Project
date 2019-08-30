0a2c8a63e3808b7fe13ddd184fbaafe23a429f59
Mike <mmcshane10@gmail.com>
2019-08-30 09:35:10 -0700

    add form reset method to js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 // BUSINESS LOGIC -----------------------------------------
 
+//Empty Arrays for form selections
+var meatInput = [];
+var toppingsInput = [];
+
 // Pizza Constructor
 function Pizza(size, meat, toppings) {
   this.size = size,
@@ -8,8 +12,35 @@ function Pizza(size, meat, toppings) {
   this.price = 0
 };
 
-var meatInput = [];
-var toppingsInput = [];
+Pizza.prototype.priceCalculator = function() {
+  if (this.size === "Small"){
+    this.price += 12;
+  } else if (this.size === "Medium"){
+    this.price += 15;
+  } else if (this.size === "Large"){
+    this.price += 18;
+  } else {
+    this.price += 20;
+  }
+
+  for (var i=0; i< this.meat.length; i++) {
+    if (this.meat[i] === "Bacon"){
+      this.price += 3;
+    } else {
+      this.price += 2;
+    }
+  };
+
+  for (var i=0; i< this.toppings.length; i++) {
+    if (this.toppings[i] === "Extra Cheese"){
+    this.price += 1;
+    } else {
+    this.price += 0.5;
+    }
+  };
+
+  return this.price;
+};
 
 // USER LOGIC ---------------------------------------------
 
@@ -30,6 +61,10 @@ $(document).ready(function() {
     });
 
     var newPizza = new Pizza(sizeInput, meatInput, toppingsInput);
+
+    newPizza.priceCalculator();
+
+    $("#pizza_builder")[0].reset();
 
     console.log(newPizza);
   });
